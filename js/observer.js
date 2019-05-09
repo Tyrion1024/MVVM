9a66101c23320151a5c931c187662fd4a8bddd24
apple <apple@appledeMacBook-Pro.local>
2019-05-09 23:26:40 +0800

    可耻的代码搬运...

--- js/observer.js
+++ js/observer.js
@@ -1,3 +1,87 @@
+/**
+ * 
+ * Observer是一个数据监听器，其实现核心方法就是前文所说的Object.defineProperty( )。如果要对所有属性都进行监听的话，那么可以通过递归方法遍历所有属性值，并对其进行Object.defineProperty( )处理。如下代码，实现了一个Observer。
+
+function defineReactive(data, key, val) {
+    observe(val); // 递归遍历所有子属性
+    Object.defineProperty(data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            return val;
+        },
+        set: function(newVal) {
+            val = newVal;
+            console.log('属性' + key + '已经被监听了，现在值为：“' + newVal.toString() + '”');
+        }
+    });
+}
+ 
+function observe(data) {
+    if (!data || typeof data !== 'object') {
+        return;
+    }
+    Object.keys(data).forEach(function(key) {
+        defineReactive(data, key, data[key]);
+    });
+};
+ 
+var library = {
+    book1: {
+        name: ''
+    },
+    book2: ''
+};
+observe(library);
+library.book1.name = 'vue权威指南'; // 属性name已经被监听了，现在值为：“vue权威指南”
+library.book2 = '没有此书籍';  // 属性book2已经被监听了，现在值为：“没有此书籍”
+
+
+思路分析中，需要创建一个可以容纳订阅者的消息订阅器Dep，订阅器Dep主要负责收集订阅者，然后再属性变化的时候执行对应订阅者的更新函数。所以显然订阅器需要有一个容器，这个容器就是list，将上面的Observer稍微改造下，植入消息订阅器：
+
+
+function defineReactive(data, key, val) {
+    observe(val); // 递归遍历所有子属性
+    var dep = new Dep(); 
+    Object.defineProperty(data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            if (是否需要添加订阅者) {
+                dep.addSub(watcher); // 在这里添加一个订阅者
+            }
+            return val;
+        },
+        set: function(newVal) {
+            if (val === newVal) {
+                return;
+            }
+            val = newVal;
+            console.log('属性' + key + '已经被监听了，现在值为：“' + newVal.toString() + '”');
+            dep.notify(); // 如果数据变化，通知所有订阅者
+        }
+    });
+}
+ 
+function Dep () {
+    this.subs = [];
+}
+Dep.prototype = {
+    addSub: function(sub) {
+        this.subs.push(sub);
+    },
+    notify: function() {
+        this.subs.forEach(function(sub) {
+            sub.update();
+        });
+    }
+};
+
+从代码上看，我们将订阅器Dep添加一个订阅者设计在getter里面，这是为了让Watcher初始化进行触发，因此需要判断是否要添加订阅者，至于具体设计方案，下文会详细说明的。在setter函数里面，如果数据变化，就会去通知所有订阅者，订阅者们就会去执行对应的更新的函数。到此为止，一个比较完整Observer已经实现了，接下来我们开始设计Watcher。
+ * 
+ */
+
+
 function Observer(data) {
     this.data = data;
     this.walk(data);
--- js/watcher.js
+++ js/watcher.js
@@ -1,3 +1,139 @@
+/**
+ * 
+订阅者Watcher在初始化的时候需要将自己添加进订阅器Dep中，那该如何添加呢？我们已经知道监听器Observer是在get函数执行了添加订阅者Wather的操作的，所以我们只要在订阅者Watcher初始化的时候触发对应的get函数去执行添加订阅者操作即可，那要如何触发get的函数，再简单不过了，只要获取对应的属性值就可以触发了，核心原因就是因为我们使用了Object.defineProperty( )进行数据监听。这里还有一个细节点需要处理，我们只要在订阅者Watcher初始化的时候才需要添加订阅者，所以需要做一个判断操作，因此可以在订阅器上做一下手脚：在Dep.target上缓存下订阅者，添加成功后再将其去掉就可以了。订阅者Watcher的实现如下：
+
+
+function Watcher(vm, exp, cb) {
+    this.cb = cb;
+    this.vm = vm;
+    this.exp = exp;
+    this.value = this.get();  // 将自己添加到订阅器的操作
+}
+ 
+Watcher.prototype = {
+    update: function() {
+        this.run();
+    },
+    run: function() {
+        var value = this.vm.data[this.exp];
+        var oldVal = this.value;
+        if (value !== oldVal) {
+            this.value = value;
+            this.cb.call(this.vm, value, oldVal);
+        }
+    },
+    get: function() {
+        Dep.target = this;  // 缓存自己
+        var value = this.vm.data[this.exp]  // 强制执行监听器里的get函数
+        Dep.target = null;  // 释放自己
+        return value;
+    }
+};
+
+这时候，我们需要对监听器Observer也做个稍微调整，主要是对应Watcher类原型上的get函数。需要调整地方在于defineReactive函数：
+
+
+function defineReactive(data, key, val) {
+    observe(val); // 递归遍历所有子属性
+    var dep = new Dep(); 
+    Object.defineProperty(data, key, {
+        enumerable: true,
+        configurable: true,
+        get: function() {
+            if (Dep.target) {.  // 判断是否需要添加订阅者
+                dep.addSub(Dep.target); // 在这里添加一个订阅者
+            }
+            return val;
+        },
+        set: function(newVal) {
+            if (val === newVal) {
+                return;
+            }
+            val = newVal;
+            console.log('属性' + key + '已经被监听了，现在值为：“' + newVal.toString() + '”');
+            dep.notify(); // 如果数据变化，通知所有订阅者
+        }
+    });
+}
+Dep.target = null;
+
+到此为止，简单版的Watcher设计完毕，这时候我们只要将Observer和Watcher关联起来，就可以实现一个简单的双向绑定数据了。因为这里没有还没有设计解析器Compile，所以对于模板数据我们都进行写死处理，假设模板上又一个节点，且id号为'name'，并且双向绑定的绑定的变量也为'name'，且是通过两个大双括号包起来（这里只是为了演示，暂时没什么用处），模板如下：
+
+<body>
+    <h1 id="name">{{name}}</h1>
+</body>
+这时候我们需要将Observer和Watcher关联起来：
+
+
+function SelfVue (data, el, exp) {
+    this.data = data;
+    observe(data);
+    el.innerHTML = this.data[exp];  // 初始化模板数据的值
+    new Watcher(this, exp, function (value) {
+        el.innerHTML = value;
+    });
+    return this;
+}
+
+然后在页面上new以下SelfVue类，就可以实现数据的双向绑定了：
+
+
+<body>
+    <h1 id="name">{{name}}</h1>
+</body>
+<script src="js/observer.js"></script>
+<script src="js/watcher.js"></script>
+<script src="js/index.js"></script>
+<script type="text/javascript">
+    var ele = document.querySelector('#name');
+    var selfVue = new SelfVue({
+        name: 'hello world'
+    }, ele, 'name');
+ 
+    window.setTimeout(function () {
+        console.log('name值改变了');
+        selfVue.data.name = 'canfoo';
+    }, 2000);
+ 
+</script>
+
+这时候打开页面，可以看到页面刚开始显示了是'hello world'，过了2s后就变成'canfoo'了。到这里，总算大功告成一半了，但是还有一个细节问题，我们在赋值的时候是这样的形式 '  selfVue.data.name = 'canfoo'  ' 而我们理想的形式是'  selfVue.name = 'canfoo'  '为了实现这样的形式，我们需要在new SelfVue的时候做一个代理处理，让访问selfVue的属性代理为访问selfVue.data的属性，实现原理还是使用Object.defineProperty( )对属性值再包一层：
+
+
+function SelfVue (data, el, exp) {
+    var self = this;
+    this.data = data;
+ 
+    Object.keys(data).forEach(function(key) {
+        self.proxyKeys(key);  // 绑定代理属性
+    });
+ 
+    observe(data);
+    el.innerHTML = this.data[exp];  // 初始化模板数据的值
+    new Watcher(this, exp, function (value) {
+        el.innerHTML = value;
+    });
+    return this;
+}
+ 
+SelfVue.prototype = {
+    proxyKeys: function (key) {
+        var self = this;
+        Object.defineProperty(this, key, {
+            enumerable: false,
+            configurable: true,
+            get: function proxyGetter() {
+                return self.data[key];
+            },
+            set: function proxySetter(newVal) {
+                self.data[key] = newVal;
+            }
+        });
+    }
+}
+ */
+
+
 function Watcher(vm, exp, cb) {
     this.cb = cb;
     this.vm = vm;
